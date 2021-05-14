d4b240a9debea4b9bfd4bc0faecc784cf8867915
panjie.dev <msowoya.panji@gmail.com>
2021-05-14 21:49:28 +0200

    added connection php

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="payment";
+
+$con=mysqli_connect ($servername,$username,$password,$dbname);
+if(!$con){
+    die("connection failed :".mysqli_connect_error());
+}
