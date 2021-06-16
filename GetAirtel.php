2dc191d4b9ec3ef8783b05802b97c77de2f4c50c
panjie.dev <msowoya.panji@gmail.com>
2021-06-16 10:49:37 +0200

    added mpamba and airtelmoney php files

---
+++ GetAirtel.php
@@ -0,0 +1,105 @@
+<?php
+
+$hostname = '{imap.gmail.com:993/imap/ssl}INBOX';
+$username = 'lawsadmission@gmail.com'; //your email here
+$password = 'laws2021'; // your password here
+
+//try to connect 
+$inbox = imap_open($hostname,$username,$password) or die('Cannot connect to Gmail: ' . imap_last_error());
+
+// grab emails 
+$emails = imap_search($inbox,'ALL');
+
+//if emails are returned, cycle through each... 
+if($emails) {
+
+rsort($emails);
+
+foreach($emails as $email_number) {
+    
+    // get information specific to this email
+$headers =imap_fetch_overview($inbox, $email_number,0);
+
+$message=imap_fetchbody($inbox,$email_number,'1');
+$submessage=substr($message,0,700);
+$finalMessage=trim(quoted_printable_decode($submessage));
+
+             require 'conn.php'; 
+          
+             //grab amount and reference number from email 
+           preg_match_all('#Amount:([ 0-9,.]*)#is', $finalMessage, $matches);// change "#Amount" to "AMT" if you want to capture agent payments
+           preg_match_all('#Ref:(.*)Bal#is', $finalMessage, $matchRef);
+   
+           foreach ($matchRef[1] as $output) {
+                   $reference1 = strip_tags("$output"); //remove white spaces
+                   $reference = trim($reference1);
+                   
+                
+                }
+   
+   
+           foreach ($matches[1] as $output2) {
+                   $amount = ("$output2");
+                  
+   
+                   
+
+                    
+           }
+   
+           echo "Payment page";
+           
+           $sql= "INSERT INTO `payments`(`Amount`, `Reference`, `status`) VALUES ('$amount','$reference', 'new')";
+         
+                
+                 mysqli_query($con, $sql);
+   
+
+            //} End foreach 
+          
+    
+    
+    
+    
+       // preg_match('#Amount:([ 0-9,.]*)#is', $msg, $matches);
+       // preg_match('#Ref:(.*)Bal#is', $msg, $matchRef);
+    
+       // preg_match('#Amount:([ 0-9,.]*)#is', $msg, $matches);
+       // preg_match('#Ref:(.*)Bal#is', $msg, $matchRef);
+        
+            }
+        
+        }
+       
+        imap_close($inbox);
+
+
+        ?>
+
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+
+        <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+         <link rel="stylesheet" type="text/css" href="./css/main.css">
+    </head>
+    <body>
+
+         <form>
+              <div class="form-group row">
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" placeholder="reference number" id="inputRef">
+                </div>
+              </div>
+               <button type="button" class="btn btn-primary">Confirm payment</button>
+        </form>
+
+    </body>
+
+
+<script href="./js/jquery.min.css" ></script>
+
+    </html>
--- home.php
+++ home.php
@@ -1,6 +1,8 @@
 <?php 
 
+require './includes/config.php';
 
+session_start();
 
 
 
@@ -17,6 +19,7 @@
  	<title>Payment page</title>
 
  	<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+ 	<link rel="stylesheet" type="text/css" href="./css/main.css">
 
  </head>
  <body>
@@ -25,25 +28,91 @@
 <h3>Payment page</h3>
 
 
-<form>
-  <div class="form-row align-items-center">
-    <div class="col-auto my-1">
-      <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
-      <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-        <option disabled selected hidden >choose payment</option>
-        <option value="1">Mpamba</option>
-        <option value="2">Airtel money</option>
-      </select>
-    </div>
-    <div class="col-auto my-1">
-      <button type="submit" class="btn btn-primary">Change</button>
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary callMpamba" data-toggle="modal" data-target="#Mpamba">
+  Mpamba
+</button>
+<button type="button" class="btn btn-primary callAirtel" data-toggle="modal" data-target="#Airtel">
+  Airtel Money
+</button>
+
+<!-- Modal 1-->
+<div class="modal fade" id="Airtel" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Airtel money</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+			  <div class="form-group row">
+			    <div class="col-sm-10">
+			      <input type="text" class="form-control" placeholder="reference number" id="inputPassword">
+			    </div>
+			  </div>
+			   <button type="button" class="btn btn-primary">Confirm payment</button>
+		</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+     
+      </div>
     </div>
   </div>
-</form>
+</div>
+
+<!-- Modal 2-->
+
+<div class="modal fade" id="Mpamba" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">TNM Mpamba</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-group row">
+          <div class="col-sm-10">
+            <input type="text" class="form-control" placeholder="reference number" id="inputPassword">
+          </div>
+        </div>
+         <button type="button" class="btn btn-primary">Confirm payment</button>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+       
+      </div>
+    </div>
+  </div>
+</div>
 
  </body>
 <script src="./js/jquery.min.js"></script>
 <script src="./js/bootstrap.min.js" ></script>
+
+
+<script>
+$('.callAirtel').on('click',function(){
+    $('.modal-body').load('GetAirtel.php',function(){
+        $('#myModal').modal({show:true});
+    });
+});
+</script>
+
+
+<script>
+$('.callMpamba').on('click',function(){
+    $('.modal-body').load('GetMpamba.php',function(){
+        $('#myModal').modal({show:true});
+    });
+});
+</script>
 
 
  </html>
